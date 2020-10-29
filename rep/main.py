ccfb8419f41836014cdbae380434a04aad58cd03
Wojciech Bogócki <wojciechbogocki@fastmail.com>
2020-10-29 22:04:05 +0800

    init command

--- rep/main.py
+++ rep/main.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from pathlib import Path
 import tinydb
 import typer
@@ -10,31 +11,41 @@ def find_rep_dir(_current=Path(".").resolve()):
     test = path.joinpath(".rep")
     if test.exists() and test.is_dir():
         return test
+    elif path == path.parent:
+        typer.echo("Database not found!")
+        raise typer.Abort()
     else:
-        if path == path.parent:
-            return None
-        else:
-            return find_rep_dir(path.parent)
+        return find_rep_dir(path.parent)
 
 
 @app.command()
 def init():
-    dir = find_rep_dir()
-    print(f"{dir}")
+    try:
+        rep_dir = Path(".rep")
+        rep_dir.mkdir()
+        db_dir = rep_dir.joinpath("database.json")
+        db = tinydb.TinyDB(db_dir)
+        db.insert({"type": "metadata", "init_time": datetime.now().isoformat()})
+    except FileExistsError:
+        typer.echo("Database already exists!")
+        raise typer.Abort()
 
 
 @app.command()
 def start(note: str = None):
+    dir = find_rep_dir()
     print(f"stop {note}")
 
 
 @app.command()
 def stop(note: str = None):
+    dir = find_rep_dir()
     print(f"stop {note}")
 
 
 @app.command()
 def note(note: str):
+    dir = find_rep_dir()
     print(f"note {note}")
 
 
