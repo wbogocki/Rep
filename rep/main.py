45b1a8460a466c0493dbaa7cdb1b97769fafe3ed
Wojciech Bogócki <wojciechbogocki@fastmail.com>
2020-10-29 18:04:55 +0800

    find .rep directory

--- rep/main.py
+++ rep/main.py
@@ -1,10 +1,42 @@
+from pathlib import Path
 import tinydb
 import typer
 
+app = typer.Typer()
 
-def main(name: str):
-    print(f"Hello, {name}!")
+
+def find_rep_dir(_current=Path(".").resolve()):
+    path = _current
+    test = path.joinpath(".rep")
+    if test.exists() and test.is_dir():
+        return test
+    else:
+        if path == path.parent:
+            return None
+        else:
+            return find_rep_dir(path.parent)
+
+
+@app.command()
+def init():
+    dir = find_rep_dir()
+    print(f"{dir}")
+
+
+@app.command()
+def start(note: str = None):
+    print(f"stop {note}")
+
+
+@app.command()
+def stop(note: str = None):
+    print(f"stop {note}")
+
+
+@app.command()
+def note(note: str):
+    print(f"note {note}")
 
 
 if __name__ == "__main__":
-    typer.run(main)
+    app()
